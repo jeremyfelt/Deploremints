bfd1470afe43f54c38d54fd46acb427abaa18b81
jeremyfelt <jeremy.felt@gmail.com>
2013-05-11 10:26:47 -0700

    Add Error Logging

--- index.php
+++ index.php
@@ -5,31 +5,43 @@
  */
 
 // Requests always come from the same IP address
-if ( ! isset( $_SERVER['REMOTE_ADDR'] ) || '63.246.22.222' !== $_SERVER['REMOTE_ADDR'] )
+if ( ! isset( $_SERVER['REMOTE_ADDR'] ) || '63.246.22.222' !== $_SERVER['REMOTE_ADDR'] ) {
+	error_log( 'Deploy Attempt Failed: Incorrect IP' );
 	die();
+}
 
 // Requests always have the same user agent
-if ( ! isset( $_SERVER['HTTP_USER_AGENT'] ) || 'Bitbucket.org' !== $_SERVER['HTTP_USER_AGENT'] )
+if ( ! isset( $_SERVER['HTTP_USER_AGENT'] ) || 'Bitbucket.org' !== $_SERVER['HTTP_USER_AGENT'] ) {
+	error_log( 'Deploy Attempt Failed: Incorrect User Agent' );
 	die();
+}
 
 // Requests always have the same content type header
-if ( ! isset( $_SERVER['CONTENT_TYPE'] ) || 'application/x-www-form-urlencoded' !== $_SERVER['CONTENT_TYPE'] )
+if ( ! isset( $_SERVER['CONTENT_TYPE'] ) || 'application/x-www-form-urlencoded' !== $_SERVER['CONTENT_TYPE'] ) {
+	error_log( 'Deploy Attempt Failed: Incorrect Content Type' );
 	die();
+}
 
 // And requests always have a payload
-if ( ! isset( $_POST['payload'] ) || empty( $_POST['payload'] ) )
+if ( ! isset( $_POST['payload'] ) || empty( $_POST['payload'] ) ) {
+	error_log( 'Deploy Attempt Failed: Payload Missing' );
 	die();
+}
 
 // So get that payload and turn it into something easy to use
 $data = json_decode( $_POST['payload'] );
 
 // If a valid response can't be given, we'll see NULL
-if ( is_null( $data ) )
+if ( is_null( $data ) ) {
+	error_log( 'Deploy Attempt Failed: Invalid JSON Response' );
 	die();
+}
 
 // We should just go home at this point
-if ( ! isset( $data->repository->website->name ) )
+if ( ! isset( $data->repository->website->name ) ) {
+	error_log( 'Deploy Attempt Failed: Unexpected Object Structure' );
 	die();
+}
 
 /**
  * Now get down to business and assume the rest of the json -> object
@@ -48,5 +60,9 @@ if ( 'jeremyfelt.com' === $data->repository->website->name ) {
 	if ( $continue_deploy ) {
 		// initiate scary stuff here and cross fingers
 		exec( 'cd /srv/www/deploy-stage; ./jf_deploy.sh' );
+	} else {
+		error_log( 'Deploy Skip: No Deploy Attempt Found' );
 	}
+} else {
+	error_log( 'Deploy Skip: Mismatched Domain' );
 }
