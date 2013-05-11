edc7da5857996581d9dda0709e536c36996a7a8c
jeremyfelt <jeremy.felt@gmail.com>
2013-05-11 12:41:03 -0700

    Auto commit and push Twenty Thirteen changes

--- deploy-stage/deploy-jeremyfelt.com.sh
+++ deploy-stage/deploy-jeremyfelt.com.sh
@@ -14,12 +14,21 @@ git pull origin master
 chmod -R g+w *
 
 # Update WordPress specifically and any other submodules that may exist
-git submodule foreach git pull
+git submodule foreach git pull origin master
 
-# As we're in active development of Twenty Thirteen, move any new changes
-# from trunk to the true content directory for the site. We'll need to
-# push changes to the repository later.
+# As Twenty Thirteen is in active development, move any new changes from
+# trunk to the true content directory for the site. We'll need to push
+# changes to the repository later.
 rsync -rv --delete /srv/www/deploy-stage/jeremyfelt.com/wordpress/wp-content/themes/twentythirteen/* /srv/www/deploy-stage/jeremyfelt.com/content/themes/twentythirteen/
+
+# Get those theme changes in the repository
+local THEME_CHANGES=""
+THEME_CHANGES=`git status | grep twentythirteen`
+if [ "$THEME_CHANGES" != "" ]; then
+    git add content/themes/twentythirteen/*
+    git commit -m "Updated Twenty Thirteen files - auto commit"
+    git push origin master
+fi
 
 # Deploy web files
 rsync -rv --delete --exclude '.git' --exclude 'external-plugins' --exclude 'content/images' --exclude 'content/uploads' /srv/www/deploy-stage/jeremyfelt.com/* /srv/www/jeremyfelt.com/
