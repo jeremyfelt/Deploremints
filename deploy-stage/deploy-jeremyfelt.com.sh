1409c3e66d714ffcff4f8ab8ec4ebd0ef997e529
jeremyfelt <jeremy.felt@gmail.com>
2013-05-11 12:04:41 -0700

    Update deploy script to actually deploy things

--- deploy-stage/deploy-jeremyfelt.com.sh
+++ deploy-stage/deploy-jeremyfelt.com.sh
@@ -12,3 +12,16 @@ git pull origin master
 
 # Address Jeremy's annoyances at files being group read-only
 chmod -R g+w *
+
+# Update WordPress specifically and any other submodules that may exist
+git submodule foreach git pull
+
+# As we're in active development of Twenty Thirteen, move any new changes
+# from trunk to the true content directory for the site. We'll need to
+# push changes to the repository later.
+rsync -rv --delete /srv/www/deploy-stage/jeremyfelt.com/wordpress/wp-content/themes/twentythirteen/* /srv/www/deploy-stage/jeremyfelt.com/content/themes/twentythirteen/
+
+# Deploy web files
+rsync -rv --delete --exclude '.git' --exclude 'external-plugins' --exclude 'content/images' --exclude 'content/uploads' /srv/www/deploy-stage/jeremyfelt.com/* /srv/www/jeremyfelt.com/
+
+# Need a hook to flush caches as the www-data user...
